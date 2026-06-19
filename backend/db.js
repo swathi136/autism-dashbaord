bd0fc6ae60c5f337cba07ce5819ef2d493d7afc7
Shruthi V <shruthisvenkatesan@outlook.com>
2026-06-19 17:43:20 +0530

    db-pool

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,24 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.error("DB connection failed:", err);
+    console.error("DB pool connection failed:", err);
     return;
   }
-  console.log("MySQL Connected");
+  console.log("MySQL Pool Connected");
+  connection.release();
 });
 
 module.exports = db;
